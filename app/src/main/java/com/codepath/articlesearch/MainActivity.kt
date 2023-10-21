d3f20921c4422655a1bb68be98fd908f62405be4
arlarv <arlonarvesu@gmail.com>
2023-10-20 23:10:12 -0400

    Somewhat working, just 1 (kind of big bug)

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,12 +3,14 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -23,21 +25,39 @@ private const val TAG = "MainActivity/"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+    private val fragment1: Fragment = ArticleListFragment()
+    private val fragment2: Fragment = BestSellerBooksFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        // Initialize bottomNavigationView after setting the content view
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavigation
+
         // Call helper method to swap the FrameLayout with the fragment
         replaceFragment(ArticleListFragment())
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.ArticlesItem -> fragment = fragment1
+                R.id.BooksItem -> fragment = fragment2
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.article_frame_layout, fragment).commit()
+            true
+        }
     }
+
     private fun replaceFragment(articleListFragment: ArticleListFragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.article_frame_layout, articleListFragment)
         fragmentTransaction.commit()
     }
+
+
 }
